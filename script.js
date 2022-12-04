3b962f13c41afd0c23503cf66b1e7a1c051a2e7a
joethyr <joethayr@gmail.com>
2022-12-03 22:12:39 -0700

    make form data populate on table

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const myLibrary = [];
-const table = document.querySelector("table");
-const tableRow = document.createElement("tr");
+const table = document.querySelector("tbody");
 const addBookBtn = document.querySelector(".add-book-btn");
 const submitBookBtn = document.querySelector(".submit-book-btn");
+const form = document.querySelector("form");
 
 function Book(title, author, pages, read) {
   this.title = title;
@@ -10,35 +10,44 @@ function Book(title, author, pages, read) {
   this.pages = pages;
   this.read = read;
 }
+
 Book.prototype.info = function () {
   alert(`${title} by ${author}, ${295} pages, ${read}.`);
 };
 
 function addBookToLibrary(book) {
   myLibrary.push(book);
+  displayBook(book);
 }
 
-const hobbit = new Book("The Hobbit", "JRR Tolkien", 295, "finished reading");
-addBookToLibrary(hobbit);
-
-// function that displays books
-myLibrary.forEach((book) => {
+function displayBook(book) {
+  let tableRow = table.appendChild(document.createElement("tr"));
   for (const key in book) {
     if (book.hasOwnProperty(key)) {
       const tableHeader = document.createElement("th");
-      let thText = document.createTextNode(`${hobbit[key]}`);
+      let thText = document.createTextNode(`${book[key]}`);
       table.appendChild(tableRow).appendChild(tableHeader).appendChild(thText);
     }
   }
-});
+}
 
-// opens Book form
 addBookBtn.addEventListener("click", () => {
   document.getElementById("bookForm").style.display = "block";
 });
 
-// submits book form
-submitBookBtn.addEventListener("submit", () => {
-  alert("it works!");
-  return false;
-});
+submitBookBtn.addEventListener("submit", createBook);
+
+function createBook(e) {
+  e.preventDefault();
+  const formData = new FormData(form);
+  const arr = [];
+  for (key of formData) {
+    arr.push(key[1]);
+  }
+  console.log(...arr);
+  newBook = new Book(...arr);
+  addBookToLibrary(newBook);
+}
+
+const hobbit = new Book("The Hobbit", "JRR Tolkien", 295, "yes");
+addBookToLibrary(hobbit);
